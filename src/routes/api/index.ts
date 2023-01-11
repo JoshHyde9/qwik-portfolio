9aed020b219906570f37b24ae2116319029050ac
JoshHyde9 <joshhyde546@gmail.com>
2023-01-11 21:43:32 +1100

    fix: is this gonna work???

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,4 +1,5 @@
 import type { RequestHandler } from "@builder.io/qwik-city";
+import { type RequestHandlerNetlify } from "@builder.io/qwik-city/middleware/netlify-edge";
 import { type InsertResult } from "kysely";
 import { v4 } from "uuid";
 import { ZodError } from "zod";
@@ -6,7 +7,15 @@ import { ZodError } from "zod";
 import { db, type GuestBook } from "~/server/db";
 import { createGuest, type GuestData } from "~/util/schema";
 
-export const onGet: RequestHandler<GuestBook[]> = async () => {
+// export const onGet: RequestHandler<GuestBook[]> = async () => {
+//   return await db
+//     .selectFrom("GuestBook")
+//     .selectAll()
+//     .orderBy("createdAt", "desc")
+//     .execute();
+// };
+
+export const onGet: RequestHandlerNetlify<GuestBook[]> = async () => {
   return await db
     .selectFrom("GuestBook")
     .selectAll()
