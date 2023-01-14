005c875cb0400ec2217375b365e6ed5e2807fc24
JoshHyde9 <joshhyde546@gmail.com>
2023-01-15 10:04:43 +1100

    feat: I cannot make my mind up

--- middleware.ts
+++ middleware.ts
@@ -9,7 +9,7 @@ const redis = new Redis({
 
 const ratelimit = new Ratelimit({
   redis,
-  limiter: Ratelimit.slidingWindow(5, "10 s"),
+  limiter: Ratelimit.slidingWindow(5, "5 s"),
 });
 
 export default async function middleware(
