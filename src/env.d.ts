00eb013d0531d45b56bd4f28c462c487ae302441
JoshHyde9 <joshhyde546@gmail.com>
2023-01-14 17:41:20 +1100

    feat: I have no idea if this is going to work

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,8 @@ interface ImportMetaEnv {
   readonly DATABASE_URL: string;
   readonly VITE_DATABASE_USERNAME: string;
   readonly VITE_DATABASE_PASSWORD: string;
+  readonly VITE_UPSTASH_REDIS_REST_URL: string;
+  readonly VITE_UPSTASH_REDIS_REST_TOKEN: string;
 }
 
 interface ImportMeta {
