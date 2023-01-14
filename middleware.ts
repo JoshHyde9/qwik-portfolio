9d3984b96019c0576b4e1e36b26f93fb32d477fc
JoshHyde9 <joshhyde546@gmail.com>
2023-01-15 09:56:22 +1100

    feat: imma just leave it like this for now

--- middleware.ts
+++ middleware.ts
@@ -18,7 +18,5 @@ export default async function middleware(
   const ip = ipAddress(request) ?? "127.0.0.1";
   const { success } = await ratelimit.limit(ip);
 
-  return success
-    ? next()
-    : rewrite((new URL("/api/blocked", request.url).pathname = "/api/blocked"));
+  return success ? next() : rewrite(new URL("/api/blocked", request.url));
 }
